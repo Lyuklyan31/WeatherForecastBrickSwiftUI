b1bc0010214964a1fc0f830d88adcf35b3a01184
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-03 19:34:32 +0200

    Created project.

---
+++ WeatherForecastBrick/WeatherForecastBrick/WeatherForecastBrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastBrickApp.swift
+//  WeatherForecastBrick
+//
+//  Created by Mac on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastBrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
