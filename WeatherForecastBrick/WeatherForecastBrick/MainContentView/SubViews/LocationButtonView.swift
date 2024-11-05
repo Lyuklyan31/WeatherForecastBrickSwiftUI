0059ddb961205163a1bb198ab45f21a3083310e0
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-05 03:51:07 +0200

    Put the button in a separate section.

---
+++ WeatherForecastBrick/WeatherForecastBrick/MainContentView/SubViews/LocationButtonView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct LocationButtonView: View {
+    var viewModel: WeatherLocationViewModel
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Image(.placeArrow)
+                    .padding(.trailing, 5)
+                
+                Text("\(viewModel.city.cityName), \(viewModel.city.countryName)")
+                    .foregroundColor(.black)
+                    .font(Font.custom("Ubuntu-Bold", size: 17))
+                    .lineLimit(0)
+                
+                Image(.magnifyingGlass)
+                    .padding(.leading, 5)
+            }
+            .padding(.horizontal)
+        }
+        .padding(.bottom, 30)
+    }
+}
